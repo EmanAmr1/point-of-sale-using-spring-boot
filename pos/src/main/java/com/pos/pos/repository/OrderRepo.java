37332146a37a0a553cbb11fef35a08512caf775c
EmanAmr1 <emanamr543@gmail.com>
2024-05-26 16:47:26 +0300

    pos Repositories

---
+++ pos/src/main/java/com/pos/pos/repository/OrderRepo.java
@@ -0,0 +1,8 @@
+package com.pos.pos.repository;
+
+import com.pos.pos.entity.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepo extends JpaRepository<OrderEntity,Integer>{
+
+}
---
+++ pos/src/main/java/com/pos/pos/repository/SaleRepo.java
@@ -0,0 +1,8 @@
+package com.pos.pos.repository;
+
+import com.pos.pos.entity.SaleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepo  extends JpaRepository<SaleEntity,Integer> {
+
+}
